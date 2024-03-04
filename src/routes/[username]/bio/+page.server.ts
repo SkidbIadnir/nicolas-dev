0cf48a343ab09c66a4f667dabf74db6be8440ed6
SkidbIadnir <n.jurdyc@gmail.com>
2024-03-04 17:17:39 +0100

    add: edit bio page

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from "./$types";
 import { adminAuth, adminDB } from "$lib/server/admin";
 import { auth } from "$lib/firebase";
 import { signOut } from "firebase/auth";
-import { error } from "@sveltejs/kit";
+import { error, fail, type Actions } from "@sveltejs/kit";
 
 export const load = (async ({ locals, params }) => {
 
@@ -20,3 +20,27 @@ export const load = (async ({ locals, params }) => {
     }
     
   }) satisfies PageServerLoad;
+
+
+export const actions = {
+    default: async ({ locals, request, params}) => {
+            const uid = locals.userID;
+            if (!uid) {
+                    throw error(401, "Unauthorized");
+            }
+            const data = await request.formData();
+            const userRef = adminDB.collection("users").doc(uid!);
+            const bio = data.get("bio");
+            const { username } = (await userRef.get()).data()!;
+            if (params.username !== username) {
+                    throw error(401, "Unauthorized");
+            }
+
+            if (bio!.toString().length > 260) {
+                    let excessBio = bio!.toString().length - 260;
+                    let problemString = `Bio must be less than ${excessBio} characters`;
+                    return fail(400, { problem: problemString});
+                }
+            await userRef.update({ bio });
+    },
+} satisfies Actions;
--- src/routes/[username]/bio/+page.svelte
+++ src/routes/[username]/bio/+page.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
-    
+    import type { PageData } from "../$types";
+    import { page } from "$app/stores";
+    import { enhance } from "$app/forms";
+
+    export let data: PageData;
 </script>
 
-<h1>Edit Bio</h1>
+<main class="max-w-lg prose text-center mx-auto my-6">
+    <p>
+        Curent Bio: <span class="text-info">{data.bio}</span>
+    </p>
+    <p>
+        Status Code: {$page.status}
+    </p>
+    <p class="text-error">
+        {$page.form?.problem ?? ""}
+    </p>
+
+    <form method="post" use:enhance>
+        <div class="form-control">
+            <label for="bio" class="label">
+               <span>
+                Your bio
+               </span>
+            </label>
+            <textarea name="bio" class="textarea textarea-bordered textarea-accent" value={data.bio}/>
+        </div>
+        <p id="errMessage" class="text-error">
+            
+        </p>
+        <button type="submit" class="btn btn-primary my-5">Save</button>
+    </form>
+</main>
