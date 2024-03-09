c4b1d982c02415db5348a319efc0b69c59624c55
SkidbIadnir <n.jurdyc@gmail.com>
2024-03-09 17:01:16 +0100

    add: translation Main/Header

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -2,6 +2,16 @@
   import ThemeChanger from "./ThemeChanger.svelte";
   import LanguageDropdown from "./LanguageDropdown.svelte";
   import { user, userData } from "$lib/firebase";
+  import { onMount } from "svelte";
+  import { getCookie } from "$lib/utils/cookies";
+  import translation from '$lib/translation/Header.json'
+
+  let lang: string = "en";
+  let tranTexts = translation as Record<string, Record<string, string>>;
+
+
+
+  onMount(() => { lang = getCookie("lang") || "en"; });
 
   $: u_data = $userData;
   export let logout = () => {};
@@ -28,23 +38,23 @@
       <ul
         class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
       >
-        <li><a href="#projects" class="duration-200">Projects</a></li>
+        <li><a href="#projects" class="duration-200">{tranTexts["Project"][lang]}</a></li>
         <!-- <li><a href="/skid" class="">Nicolas's links</a></li> -->
 
       </ul>
     </div>
-    <a href="/#intropage" class="btn btn-ghost text-xl">Nicolas Jurdyc</a>
+    <a href="/#intropage" class="btn btn-ghost text-xl">{tranTexts["firstName"][lang]} {tranTexts["lastName"][lang]}</a>
   </div>
   <div class="navbar-center hidden lg:flex">
     <ul class="menu menu-horizontal px-1">
       <li>
-        <a class="duration-200 font-semibold" href="/#projects">Projects</a>
+        <a class="duration-200 font-semibold" href="/#projects">{tranTexts["Project"][lang]}</a>
       </li>
       <!-- <li><a class="font-semibold" href="/skid">Nicolas's links</a></li> -->
       {#if $user}
       <li>
         <details>
-          <summary class="font-semibold">Edit</summary>
+          <summary class="font-semibold">{tranTexts["edit"][lang]}</summary>
           <ul class="p-2 z-10">
             <li><a href="/{u_data?.username}/bio" class="font-semibold">Bio</a></li>
             <li><a href="/{u_data?.username}/edit" class="font-semibold">Links</a></li>
@@ -52,7 +62,7 @@
           </ul>
         </details>
       </li>
-      <li><a href="/{u_data?.username}" class="font-semibold">My page</a></li>
+      <li><a href="/{u_data?.username}" class="font-semibold">{tranTexts["myPage"][lang]}</a></li>
 
       {/if}
     </ul>
@@ -64,10 +74,10 @@
     </div>
     {#if $user}
       <a href="/" on:click={logout} class="btn hover:text-error"
-        >Sign out<i class="fas fa-right-from-bracket"></i></a
+        >{tranTexts["signOut"][lang]}<i class="fas fa-right-from-bracket"></i></a
       >
     {:else}
-      <a href="/login" class="btn">Sign in<i class="fas fa-right-to-bracket"></i></a>
+      <a href="/login" class="btn">{tranTexts["signIn"][lang]}<i class="fas fa-right-to-bracket"></i></a>
     {/if}
   </div>
 </div>
--- src/lib/components/pages/Main.svelte
+++ src/lib/components/pages/Main.svelte
@@ -1,48 +1,75 @@
 <script lang="ts">
- import profilPic from '$lib/assets/images/profile.png';
- import background from '$lib/assets/images/backgrond.jpg';
- import svelte_logo from '$lib/assets/images/svelte_logo.png';
+  import background from "$lib/assets/images/backgrond.jpg";
+  import { onMount } from "svelte";
+  import { getCookie } from "$lib/utils/cookies";
+  import translation from "$lib/translation/Main.json";
 
+  let lang: string = "en";
+  let tranTexts = translation as Record<string, Record<string, string>>;
+  onMount(() => {
+    lang = getCookie("lang") || "en";
+  });
 
-    function scrollByAmount(amount: number) {
-        window.scrollBy({
-            top: amount,
-            behavior: 'smooth'
-        });
-    }
+  function scrollByAmount(amount: number) {
+    window.scrollBy({
+      top: amount,
+      behavior: "smooth",
+    });
+  }
 </script>
 
 <main class="flex flex-1 flex-col">
-    <section id="intropage" class="">
-        <div class="hero min-h-screen" style="background-image: url({background})">
-            <div class="hero-overlay bg-opacity-50"></div>
-            <div class="hero-content text-center text-neutral-content">
-              <div class="max-w-md">
-                <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                <p class="mb-5">My name is <span class="text-accent">Nicolas</span>, I am a fullstack developer and I love building things.</p>
-                <button class="btn btn-primary" on:click={() => {
-                    scrollByAmount(1000);
-                }} >Get Started</button>
-              </div>
-            </div>
-          </div>
-    </section>
-    <section id="projects" class="py-20 lg:p-32 flex flex-col gap-24">
-        <h2 class="text-4xl sm:text-5xl lg:text-6xl font-bold text-center">Projects</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
-            <a target="_blank" href="https://github.com/SkidbIadnir/nicolas-dev" class="card bg-base-200 p-8 rounded-lg hover:text-accent transition-colors duration-200">
-                <h3 class="text-2xl font-bold">This web site</h3>
-                <p class="text-lg">My own portfolio built with SvelteKit</p>
-            </a>
-            <a href="/gamedev" class="card bg-base-200 p-8 rounded-lg hover:text-accent transition-colors duration-200">
-                <h3 class="text-2xl font-bold">Game Development</h3>
-                <p class="text-lg">A page to showcase my game development projects</p>
-                <i class="fas fa-arrow-right card-actions justify-end"></i>
-            </a>
-            <a href="/about" class="card bg-base-200 p-8 rounded-lg hover:text-accent transition-colors duration-200">
-                <h3 class="text-2xl font-bold">Documents ?</h3>
-                <p class="text-lg">Information about me and my downloadable resume</p>
-            </a>
+  <section id="intropage" class="">
+    <div class="hero min-h-screen" style="background-image: url({background})">
+      <div class="hero-overlay bg-opacity-50"></div>
+      <div class="hero-content text-center text-neutral-content">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">{tranTexts["hello"][lang]}</h1>
+          <p class="mb-5">
+            {tranTexts["intro1"][lang]}
+            <span class="text-accent">{tranTexts["introName"][lang]}</span
+            >{tranTexts["intro2"][lang]}
+          </p>
+          <button
+            class="btn btn-primary"
+            on:click={() => {
+              scrollByAmount(1000);
+            }}>{tranTexts["buttonProjects"][lang]}</button
+          >
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
+  <section id="projects" class="py-20 lg:p-32 flex flex-col gap-24">
+    <h2 class="text-4xl sm:text-5xl lg:text-6xl font-bold text-center">
+      {tranTexts["projects"][lang]}
+    </h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+      <a
+        target="_blank"
+        href="https://github.com/SkidbIadnir/nicolas-dev"
+        class="card bg-base-200 p-8 rounded-lg hover:text-accent transition-colors duration-200"
+      >
+        <h3 class="text-2xl font-bold">{tranTexts["project1Title"][lang]}</h3>
+        <p class="text-lg">{tranTexts["project1Desc"][lang]}</p>
+        <i class="fas fa-arrow-right card-actions justify-end"></i>
+      </a>
+      <a
+        href="/gamedev"
+        class="card bg-base-200 p-8 rounded-lg hover:text-accent transition-colors duration-200"
+      >
+        <h3 class="text-2xl font-bold">{tranTexts["project2Title"][lang]}</h3>
+        <p class="text-lg">{tranTexts["project2Desc"][lang]}</p>
+        <i class="fas fa-arrow-right card-actions justify-end"></i>
+      </a>
+      <a
+        href="/about"
+        class="card bg-base-200 p-8 rounded-lg hover:text-accent transition-colors duration-200"
+      >
+        <h3 class="text-2xl font-bold">{tranTexts["project3Title"][lang]}</h3>
+        <p class="text-lg">{tranTexts["project3Desc"][lang]}</p>
+        <i class="fas fa-arrow-right card-actions justify-end"></i>
+      </a>
+    </div>
+  </section>
 </main>
